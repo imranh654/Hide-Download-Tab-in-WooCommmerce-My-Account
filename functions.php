58fd6266f886c26de20714bb8d81ba2802b393d6
Imran Hossain <umemous@gmail.com>
2018-02-03 17:31:29 +0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,5 @@
+function custom_my_account_menu_items( $items ) {
+    unset($items['downloads']);
+    return $items;
+}
+add_filter( 'woocommerce_account_menu_items', 'custom_my_account_menu_items' );
